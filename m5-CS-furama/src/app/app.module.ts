250557687e46cbeb907def7bef33f317b503928f
quynhngo2909 <dieuquynh105@gmail.com>
2023-03-07 16:23:45 +0700

    nop bt

--- m5-CS-furama/src/app/app.module.ts
+++ m5-CS-furama/src/app/app.module.ts
@@ -9,6 +9,11 @@ import { FooterComponent } from './footer/footer.component';
 import { FacilityComponent } from './facility/facility.component';
 import { FacilityCreateComponent } from './facility-create/facility-create.component';
 import { FacilityUpdateComponent } from './facility-update/facility-update.component';
+import { CustomerComponent } from './customer/customer.component';
+import { CustomerCreateComponent } from './customer-create/customer-create.component';
+import { CustomerUpdateComponent } from './customer-update/customer-update.component';
+import { ContractComponent } from './contract/contract.component';
+import { ContractCreateComponent } from './contract-create/contract-create.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +23,12 @@ import { FacilityUpdateComponent } from './facility-update/facility-update.compo
     FooterComponent,
     FacilityComponent,
     FacilityCreateComponent,
-    FacilityUpdateComponent
+    FacilityUpdateComponent,
+    CustomerComponent,
+    CustomerCreateComponent,
+    CustomerUpdateComponent,
+    ContractComponent,
+    ContractCreateComponent
   ],
   imports: [
     BrowserModule,
